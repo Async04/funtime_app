f872ea5ccf80ff6b184d2f5f64b68bb8c7798dab
SamandarToyirov <asyncdeveloper023@gmail.com>
2024-07-09 19:36:14 +0500

    database re-structed 2

--- src/main/java/com/example/funtime_app/repository/ViewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.funtime_app.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ViewRepository extends JpaRepository<View, UUID> {
-}
