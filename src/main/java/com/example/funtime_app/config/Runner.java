698f4f7ff719b92909e0628ccd03e5457f9a00f6
Jakhongir Islomjonov <jahongirnamti@gmail.com>
2024-07-23 12:31:19 +0500

    add swigger configur

--- src/main/java/com/example/funtime_app/config/Runner.java
+++ src/main/java/com/example/funtime_app/config/Runner.java
@@ -27,136 +27,119 @@ public class Runner implements CommandLineRunner {
     private final VideoRepository videoRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+
     @Override
     public void run(String... args) throws Exception {
 
 
-//        Role role1 = Role.builder()
-//                .roleName(RoleName.ROLE_ADMIN)
-//                .build();
-//        Role role2 = Role.builder()
-//                .roleName(RoleName.ROLE_USER)
-//                .build();
-//        roleRepository.save(role1);
-//        roleRepository.save(role2);
-//
-//        User user1 = User.builder()
-//                .username("user")
-//                .roles(List.of(role2))
-//                .password(passwordEncoder.encode("123"))
-//                .build();
-//          User user2 = User.builder()
-//                .username("admin")
-//                  .roles(List.of(role1))
-//                .password(passwordEncoder.encode("123"))
-//                .build();
-//
-//          userRepository.save(user1);
-//          userRepository.save(user2);
+        Role role1 = Role.builder()
+                .roleName(RoleName.ROLE_ADMIN)
+                .build();
+        Role role2 = Role.builder()
+                .roleName(RoleName.ROLE_USER)
+                .build();
+        roleRepository.save(role1);
+        roleRepository.save(role2);
+
+        User user1 = User.builder()
+                .username("user")
+                .roles(List.of(role2))
+                .password(passwordEncoder.encode("123"))
+                .build();
+        User user2 = User.builder()
+                .username("admin")
+                .roles(List.of(role1))
+                .password(passwordEncoder.encode("123"))
+                .build();
+
+        userRepository.save(user1);
+        userRepository.save(user2);
 
 
+      /*  byte[] bytes1 = Files.readAllBytes(Path.of("com/example/funtime_app/config/rasm.jpg"));
+
+        Path path = Paths.get("src/main/java/com/example/funtime_app/config/", "video.mp4");
+        byte[] bytes = Files.readAllBytes(path);
+
+        Attachment attachment = Attachment.builder()
+                .content(bytes)
+                .contentType("mp4")
+                .build();
+        attachmentRepository.save(attachment);
+        Video video = Video.builder()
+                .attachment(attachment)
+                .build();
+        videoRepository.save(video);
+
+        bytes = Files.readAllBytes(path);
+        Random random = new Random();
 
 
-        // byte[] bytes1 = Files.readAllBytes(Path.of("com/example/funtime_app/config/rasm.jpg"));
+        for (int i = 0; i < 2000; i++) {
 
-//       Path path = Paths.get("src/main/java/com/example/funtime_app/config/", "video.mp4");
-//                byte[] bytes = Files.readAllBytes(path);
-//
-//        Attachment attachment = Attachment.builder()
-//                .content(bytes)
-//                .contentType("mp4")
-//                .build();
-//        attachmentRepository.save(attachment);
-//        Video video = Video.builder()
-//               .attachment(attachment)
-//               .build();
-//        videoRepository.save(video);
+            attachment = Attachment.builder()
+                    .contentType("jpeg")
+                    .content(bytes)
+                    .build();
 
-//        byte[] bytes = Files.readAllBytes(path);
-//        Random random = new Random();
-//
-//
-//        for (int i = 0; i < 2000; i++) {
-//
-//            Attachment attachment = Attachment.builder()
-//                    .contentType("jpeg")
-//                    .content(bytes)
-//                    .build();
-//
-//            attachmentRepository.save(attachment);
-//
-//            User user = User.builder()
-//                    .email("salom"+i+"@gmail.com")
-//                    .firstName("FirstName"+i)
-//                    .lastName("LastName"+i)
-//                    .profilePhoto(attachment)
-//                    .password("Root123")
-//                    .username("username"+i)
-//                    .build();
-//            userRepository.save(user);
-//
-//        }
-//
-//        for (int i = 0; i < 50; i++) {
-//
-//            CategoryTag categoryTag = CategoryTag.builder()
-//                    .tagName("Tag"+i)
-//                    .build();
-//
-//            categoryTagRepository.save(categoryTag);
-//
-//        }
-//
-//        List<CategoryTag> tags = categoryTagRepository.findAll();
-//        List<User> users = userRepository.findAll();
-//
-//
-//        for (int i = 0; i < 10; i++) {
-//
-//            Attachment attachment = Attachment.builder()
-//                    .content(bytes)
-//                    .contentType("jpeg")
-//                    .build();
-//
-//            attachmentRepository.save(attachment);
-//
-//            Category category = Category.builder()
-//                    .name("Category"+i)
-//                    .tags(List.of(
-//                            tags.get(random.nextInt(1,49))
-//                            ))
-//                    .attachment(attachment)
-//                    .build();
-//
-//            categoryRepository.save(category);
-//
-//        }
-//
-//        List<Category> categories = categoryRepository.findAll();
-//        LocalDateTime now = LocalDateTime.now();
-//
-//        for (int i = 0; i < 10000; i++) {
-//
-//            Attachment attachment = Attachment.builder()
-//                    .content(bytes)
-//                    .contentType("jpeg")
-//                    .build();
-//
-//            attachmentRepository.save(attachment);
-//
-//            Post post = Post.builder()
-//                    .attachment(attachment)
-//                    .description("Description "+i)
-//                    .title("Title "+i)
-//                    .user(users.get(random.nextInt(1, 2000)))
-//                    .category(categories.get(random.nextInt(1,10)))
-//                    .views(random.nextInt(2000, 231234))
-//                    .createdAt(now.minusDays(random.nextInt(1, 50)))
-//                    .build();
-//
-//            postRepository.save(post);
-//
-//        }
+            attachmentRepository.save(attachment);
+
+            User user = User.builder()
+                    .email("salom" + i + "@gmail.com")
+                    .firstName("FirstName" + i)
+                    .lastName("LastName" + i)
+                    .profilePhoto(attachment)
+                    .password("Root123")
+                    .username("username" + i)
+                    .build();
+            userRepository.save(user);
+
+        }
+
+        for (int i = 0; i < 50; i++) {
+
+            CategoryTag categoryTag = CategoryTag.builder()
+                    .tagName("Tag" + i)
+                    .build();
+
+            categoryTagRepository.save(categoryTag);
+
+        }
+
+        List<CategoryTag> tags = categoryTagRepository.findAll();
+        List<User> users = userRepository.findAll();
+
+
+        for (int i = 0; i < 10; i++) {
+
+            attachment = Attachment.builder()
+                    .content(bytes)
+                    .contentType("jpeg")
+                    .build();
+
+            attachmentRepository.save(attachment);
+
+            Category category = Category.builder()
+                    .name("Category" + i)
+                    .tags(List.of(
+                            tags.get(random.nextInt(1, 49))
+                    ))
+                    .attachment(attachment)
+                    .build();
+
+            categoryRepository.save(category);
+
+        }
+
+        List<Category> categories = categoryRepository.findAll();
+        LocalDateTime now = LocalDateTime.now();
+
+        for (int i = 0; i < 10000; i++) {
+
+            attachment = Attachment.builder()
+                    .content(bytes)
+                    .contentType("jpeg")
+                    .build();
 
 //        Path path = Paths.get("src/main/java/com/example/funtime_app/config/", "video.mp4");
 //        byte[] bytes = Files.readAllBytes(path);
@@ -177,7 +160,21 @@ public class Runner implements CommandLineRunner {
 //
 //        }
 
+            attachmentRepository.save(attachment);
 
+            Post post = Post.builder()
+                    .attachment(attachment)
+                    .description("Description " + i)
+                    .title("Title " + i)
+                    .user(users.get(random.nextInt(1, 2000)))
+                    .category(categories.get(random.nextInt(1, 10)))
+                    .views(random.nextInt(2000, 231234))
+                    .createdAt(now.minusDays(random.nextInt(1, 50)))
+                    .build();
+
+            postRepository.save(post);
+
+        }*/
 
 
     }
--- target/generated-sources/annotations/com/example/funtime_app/mappers/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/funtime_app/mappers/UserMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-18T05:34:27+0500",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
+    date = "2024-07-20T12:40:01+0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.3 (Amazon.com Inc.)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
